f738a0a79962b0b25b03e5b863a2bd261f438fdd
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2023-06-03 17:03:44 +0530

    Improved Description texts.

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,12 +1,12 @@
 const { Insta,cmd } = require('../lib')
 cmd({
         pattern: "insta",
-        desc: "download instagram post.",
+        desc: "Anyone can download Instagram post by this command .",
         category: "downloader",
         filename: __filename
     },
     async(SynPika, Syn,text,{isCreator}) => {
-if(!text) return Syn.reply('Need post url.')
+if(!text) return Syn.reply('I need a insta post URL to proceed')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
 await SynPika.sendFileUrl(Syn.chat, response[i], `*Downloaded Media from instagram.*`, Syn)
